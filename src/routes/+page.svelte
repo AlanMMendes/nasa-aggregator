6acb731eba1eac6d4d0371c14ca1d39321bdd12a
Alan M. Mendes <alanmazeto.m@gmail.com>
2024-09-12 18:50:06 -0300

    Initial commit add stores and a new page for nasa-aggregator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { curiosity, fetchData } from '../stores/curiosity';
+
+  onMount(() => {
+   fetchData('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=oBKUfq4ojUgfJdXxmYCP8EAnvIWw9NtXP172FpqR'); 
+  });
+
+
+</script>
+
+<main>
+ {#if $curiosity.loading}
+  <p>Loading...</p>
+{:else if $curiosity.error}
+  <p>Error: {$curiosity.error}</p>
+{:else}
+ <div class="grid grid-cols-3 justify-center items-center">
+  {#each $curiosity.data.photos.slice(0, 10) as rover}
+  <div class="p-4 flex justify-center items-center">
+  <h1 class="text-2xl font-bold text-black">{rover.id}</h1>
+  </div>
+  {/each}
+ </div>
+{/if}
+
+</main>
+
